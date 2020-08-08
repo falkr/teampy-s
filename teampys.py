47da8a3a0eb87b073b95e6b85db943f875fb2f54
Frank Alexander Kraemer <kraemer@ntnu.no>
2020-08-08 11:48:58 +0200

    Centralized all access to global data within access methods to prepare database integration.

--- teampys.py
+++ teampys.py
@@ -7,17 +7,9 @@ from flask import send_file
 import uuid
 import random
 import string
+import json
 
 app = Flask(__name__)
-
-# all scratchcards
-cards = {}
-
-# teacher UUID to RAT
-rats_by_private_id = {}
-
-# student access to RAT
-rats_by_public_id = {}
 
 colors = ['STEELBLUE', 'CADETBLUE', 'LIGHTSEAGREEN', 'OLIVEDRAB', 
     'YELLOWGREEN', 'FORESTGREEN', 'MEDIUMSEAGREEN', 'LIGHTGREEN', 
@@ -275,7 +267,7 @@ class RAT():
             d['private_id'], d['public_id'], 
             d['label'], d['teams'], d['questions'], d['alternatives'], d['solution'], d['team_colors'])
 
-    def get_status_table(self, base_url):
+    def get_status_table(self, base_url, cards):
         s = []
         s.append('<table class="table table-sm">')
         s.append('<thead>')
@@ -288,20 +280,18 @@ class RAT():
         s.append('</tr>')
         s.append('</thead>')
         s.append('<tbody>')
-        global cards
-        for card_id in self.card_ids_by_team.values():
-            card = cards[card_id]
+        for card in cards:
             s.append(card.get_table_row(base_url))
         s.append('</tbody>')
         s.append('</table>')
         return ''.join(s)
 
-    def html_teacher(self, base_url):
+    def html_teacher(self, base_url, cards):
         s = []
         public_url = base_url + 'rat/{}'.format(self.public_id)
         private_url = base_url + 'teacher/{}'.format(self.private_id)
         download_url = base_url + 'download/{}'.format(self.private_id)
-        return render_template('rat_teacher.html', public_url=public_url, private_url=private_url, table=self.get_status_table(base_url), download_url=download_url)
+        return render_template('rat_teacher.html', public_url=public_url, private_url=private_url, table=self.get_status_table(base_url, cards), download_url=download_url)
 
     def html_students(self, base_url):
         s = []
@@ -319,17 +309,50 @@ class RAT():
             # TODO check if team exists
             return self.card_ids_by_team[team]
     
-    def download(self, format):
-        global cards
+    def download(self, format, cards):
         if format == "string":
             s = []
-            for card_id in self.card_ids_by_team.values():
-                card = cards[card_id]
+            for card in cards:
                 s.append(card.get_text_result())
             return send_file(io.BytesIO('\n'.join(s).encode('utf-8')),
                      attachment_filename='trat.txt',
                      as_attachment=True,
                      mimetype='text/text')
+
+# all scratchcards
+cards = {}
+# teacher UUID to RAT
+rats_by_private_id = {}
+# student access to RAT
+rats_by_public_id = {}
+
+def find_rat_by_public_id(public_id):
+    global rats_by_public_id
+    if public_id in rats_by_public_id:
+        return rats_by_public_id[public_id]
+    return None
+
+def find_rat_by_private_id(private_id):
+    global rats_by_private_id
+    if private_id in rats_by_private_id:
+        return rats_by_private_id[private_id]
+    return None
+
+def store_rat(rat):
+    global rats_by_private_id
+    global rats_by_public_id
+    rats_by_private_id[rat.private_id] = rat
+    rats_by_public_id[rat.public_id] = rat
+
+def find_card_by_id(card_id):
+    global cards
+    if card_id in cards:
+        return cards[card_id]
+    return None
+
+def store_card(card):
+    global cards
+    cards[card.id] = card
 
 
 @app.route('/')
@@ -338,11 +361,11 @@ def index():
     return render_template('start.html', primary='#007bff', action_url=action_url)
 
 def return_student_page(public_id):
-    global rats_by_public_id
-    if public_id in rats_by_public_id:
-        rat = rats_by_public_id[public_id]
+    rat = find_rat_by_public_id(public_id)
+    if rat is None:
+        return "Could not find rat."
+    else:
         return rat.html_students(request.host_url)
-    return "Could not find rat"
 
 @app.route('/join', methods=['POST', 'GET'])
 def join():
@@ -382,67 +405,65 @@ def create():
     public_id = ''.join(random.choices(string.ascii_uppercase, k=5))
     team_colors = random.sample(colors, teams) 
     rat = RAT(private_id, public_id, label, teams, questions, alternatives, solution, team_colors)
-    global rats_by_private_id
-    global rats_by_public_id
-    rats_by_private_id[private_id] = rat
-    rats_by_public_id[public_id] = rat
+    store_rat(rat)
     # create a new card for each team
-    global cards
     for team in range(1, int(teams) + 1, 1):
         card = Card.new_card(label, str(team), int(questions), int(alternatives), solution, rat.team_colors[team-1])
-        cards[card.id] = card
+        store_card(card)
         rat.card_ids_by_team[str(team)] = card.id
     return redirect("../teacher/{}".format(rat.private_id), code=302)
 
 @app.route('/teacher/<private_id>/')
 def show_rat_teacher(private_id):
-    global rats_by_private_id
-    if private_id in rats_by_private_id:
-        rat = rats_by_private_id[private_id]
-        return rat.html_teacher(request.host_url)
-    return "Could not find rat. Currently there are {} RATs stored.".format(len(rats_by_private_id))
+    rat = find_rat_by_private_id(private_id)
+    if rat is None:
+        return "Could not find rat."
+    rat_cards = []
+    for card_id in rat.card_ids_by_team.values():
+        card = find_card_by_id(card_id)
+        if card is not None:
+            rat_cards.append(card)
+    return rat.html_teacher(request.host_url, rat_cards)
 
 @app.route('/card/<id>/')
 def show_card(id):
-    global cards
-    if id in cards:
-        card = cards[id]
-        # check if the page request also answers a question
-        if ('question' in request.form): # and ('alternative' in request.form):
-            question = request.form['question']
-            alternative = request.form['alternative']
-            card.uncover(question, alternative)
-        if ('question' in request.args): # and ('alternative' in request.form):
-            question = request.args['question']
-            alternative = request.args['alternative']
-            card.uncover(question, alternative)
-        return card.get_card_html(request.host_url)
-    else:
-        return "Could not find rat {}".format(rats_by_public_id)
+    card = find_card_by_id(id)
+    if card is None:
+        return "Could not find card."
+    # check if the page request also answers a question
+    if ('question' in request.form): # and ('alternative' in request.form):
+        question = request.form['question']
+        alternative = request.form['alternative']
+        card.uncover(question, alternative)
+    if ('question' in request.args): # and ('alternative' in request.form):
+        question = request.args['question']
+        alternative = request.args['alternative']
+        card.uncover(question, alternative)
+    store_card(card)
+    return card.get_card_html(request.host_url)
 
 @app.route('/grab/<public_id>/<team>')
 def grab_rat_students(public_id, team):
-    global rats_by_public_id
-    app.logger.debug(rats_by_public_id)
-    app.logger.debug(public_id)
-    if public_id not in rats_by_public_id:
-        return "Could not find rat {}".format(rats_by_public_id)
-    else:
-        rat = rats_by_public_id[public_id]
-        card_id = rat.grab(team)
-        if card_id is None:
-            return 'Somebody already grabbed that card.'
-        else:
-            global cards
-            if card_id in cards:
-                return redirect("../../card/{}".format(card_id), code=302)
-            else:
-                return "Could not find card with ID {}".format(card_id)
+    rat = find_rat_by_public_id(public_id)
+    if rat is None:
+        return "Could not find RAT."
+    card_id = rat.grab(team)
+    if card_id is None:
+        return 'Somebody already grabbed that card.'
+    card = find_card_by_id(card_id)
+    if card is None:
+        return "Could not find card with ID {}".format(card_id)
+    store_rat(rat)
+    return redirect("../../card/{}".format(card_id), code=302)
 
 @app.route('/download/<private_id>/<format>/')
 def download(private_id, format):
-    global rats_by_private_id
-    if private_id in rats_by_private_id:
-        rat = rats_by_private_id[private_id]
-        return rat.download(format)
-    return "Could not find rat. Currently there are {} RATs stored.".format(len(rats_by_private_id))
+    rat = find_rat_by_private_id(private_id)
+    if rat is None:
+        return "Could not find RAT."
+    rat_cards = []
+    for card_id in rat.card_ids_by_team.values():
+        card = find_card_by_id(card_id)
+        if card is not None:
+            rat_cards.append(card)
+    return rat.download(format, rat_cards)
